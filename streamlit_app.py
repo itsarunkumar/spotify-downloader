984d15776ee7a57c6cd6d1b266139c172f2ef28b
itsarunkumar <86278327+itsarunkumar@users.noreply.github.com>
2023-02-18 18:00:43 +0000

    main changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,31 @@
-import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from pytube import Search, YouTube, Playlist
-from moviepy import editor as mp
+import streamlit as st
+import pandas as pd
+import numpy as np
+from pytube import YouTube, Search
 
-st.title("Spotify Downloader")
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+    client_id='29b30746b4ca4a3b81c326357067bebd', client_secret='a49cb1c7277647499d115c13eac55a78'))
 
-st.write("This is a simple Spotify Downloader")
+st.title('Spotify Song Downloader')
 
-url = st.text_input("Enter Spotify Playlist URL")
+st.write('Enter the name of the song you want to download')
 
-if st.button("Download"):
-    playlist_url = url
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
-        client_id='29b30746b4ca4a3b81c326357067bebd', client_secret='a49cb1c7277647499d115c13eac55a78'))
-    results = spotify.playlist(playlist_url)
-    for idx, item in enumerate(results['tracks']['items']):
-        track = item['track']
-        st.write(idx, track['artists'][0]['name'], " – ", track['name'])
-        search = Search(track['artists'][0]['name'] + " – " + track['name'])
-        if not search.results:
-            st.write("No results found")
-            continue
-        yt = YouTube(
-            search.results[0].watch_url).streams.get_by_itag(251)
+song_name = st.text_input('Song Name')
 
-        if not yt:
-            st.write("No audio found")
-            continue
+if st.button('Download'):
+    results = spotify.search(q='track:' + song_name, type='track')
+    for idx, track in enumerate(results['tracks']['items']):
+        track_name = st.write(idx, track['name'])
+        st.write(track['artists'][0]['name'])
+        st.write(track['external_urls']['spotify'])
 
-        download_file = yt.download()
-        if not download_file:
-            st.write("Download failed")
-            continue
+        search = Search(song_name)
+        url = search.results[0].watch_url
+        yt = YouTube(url).streams.get_audio_only()
+        yt_downlaoded = yt.download()
 
-        st.download_button(download_file, "Download")
+        st.write('Downloaded' + song_name + 'successfully')
 
-        st.write("Downloaded" + track['artists']
-                 [0]['name'] + " – " + track['name'])
+        st.download_button('Download', yt_downlaoded, 'mp3')
