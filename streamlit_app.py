65d2fa841356f26553feee833fa9ab658151f80e
itsarunkumar <86278327+itsarunkumar@users.noreply.github.com>
2023-02-20 17:55:04 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ from pytube import YouTube, Search
 # spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
 #     client_id='29b30746b4ca4a3b81c326357067bebd', client_secret='a49cb1c7277647499d115c13eac55a78'))
 
+st.header('Spotify Song Downloader')
+
 st.title('Spotify Song Downloader')
 
 st.write('Enter the name of the song you want to download')
